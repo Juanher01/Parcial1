07355800e4d850f47ca00f20c9a5adb9559dffc3
Juanher01 <u20222209033@usco.edu.co>
2024-03-19 10:13:41 -0500

    Agrego noticias

---
+++ Noticias.js
@@ -0,0 +1,14 @@
+function Noticia1() {
+    window.location.href = "https://www.infobae.com/america/agencias/2024/03/19/el-coi-reafirma-su-oposicion-a-la-politizacion-del-deporte-por-rusia/";
+}
+
+function Noticia2() {
+    window.location.href = "https://okdiario.com/noticias/real-madrid-club-futbol/";
+}
+
+function Noticia3() {
+    window.location.href = "https://www.fichajes.com/a4645344918742242176-el-fc-barcelona-paraliza-la-busqueda-de-nuevo-tecnico";
+}
+
+
+
